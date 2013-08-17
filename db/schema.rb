852ed1a86c09dde22302f7071d1d85c9f9ef2bd8
Ziyan Junaideen <jdeen-solutions@outlook.com>
2013-08-17 12:58:39 +0530

    Rails Migration - currency precision change

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130817063203) do
+ActiveRecord::Schema.define(version: 20130817071707) do
 
   create_table "table_records", force: true do |t|
     t.date     "date"
     t.text     "description"
-    t.decimal  "value",       precision: 10, scale: 0
+    t.decimal  "value",       precision: 8, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
